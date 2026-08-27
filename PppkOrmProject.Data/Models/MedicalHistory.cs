f15acf26cae483a8f49f3cf077284a14dbc0cefe
Domagoj <dantic1@algebra.hr>
2026-08-27 19:01:04 +0200

    add MedicalHistory entity

---
+++ PppkOrmProject.Data/Models/MedicalHistory.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PppkOrmProject.Data.Models;
+
+public class MedicalHistory
+{
+    public int Id { get; set; }
+    
+    // FKs
+    public int PatientId { get; set; }
+    public int DiseaseId { get; set; }
+    
+    [Required]
+    public DateOnly StartDate { get; set; }
+    
+    public DateOnly? EndDate { get; set; }
+
+    public Patient Patient { get; set; } = null!;
+    public Disease Disease { get; set; } = null!;
+}
