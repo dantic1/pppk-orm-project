ac7486b3c03b9d86eb1f57cf503a3c443a6a1d7e
Domagoj <dantic1@algebra.hr>
2026-08-27 18:14:56 +0200

    add Patient entity

---
+++ PppkOrmProject.Data/Models/Patient.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using PppkOrmProject.Data.Enums;
+
+namespace PppkOrmProject.Data.Models;
+
+public class Patient
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; } = String.Empty;
+    
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; } = String.Empty;
+    
+    [Required]
+    public Gender Gender { get; set; }
+    
+    [Required]
+    [MaxLength(11)]
+    public string Oib { get; set; } = String.Empty;
+    
+    [Required]
+    [MaxLength(150)]
+    public string ResidenceAddress { get; set; } = String.Empty;
+    
+    [Required]
+    [MaxLength(150)]
+    public string PermanentAddress { get; set; } = String.Empty;
+    
+}
