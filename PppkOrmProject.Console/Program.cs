d7608395ca1d9600234c662e2f361bd01fde1a2a
Domagoj <dantic1@algebra.hr>
2026-08-28 15:53:15 +0200

    check Db connection from console app

--- PppkOrmProject.Console/Program.cs
+++ PppkOrmProject.Console/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PppkOrmProject.Data;
+
+using var context = new AppDbContext();
+
+var canConnect = context.Database.CanConnect();
+
+Console.WriteLine($"DbConnection {canConnect}");
